6e581cd92ca7b525006bc038fbc689d007205bfa
Cecilia Stark <cecilia.d@ibm.com>
2021-04-06 04:17:49 -0500

    project updates

---
+++ moduleOneProjects/moms-shopping-list-start/index.js
@@ -0,0 +1,27 @@
+const form = document["addItem"]
+
+
+form.addEventListener("submit", function(event){
+    event.preventDefault()
+    let uList = document.getElementById("list")
+
+    let newItem = document.createElement("li")
+    let deleteButton = document.createElement('button')
+    let editButton = document.createElement('button')
+    let inputDiv = document.createElement('div')
+    let inputValue = document.getElementById('title').value
+
+    deleteButton.textContent = "X"
+    editButton.textContent = "Edit"
+    inputDiv.textContent = inputValue
+
+    
+    
+    newItem.append(inputDiv, deleteButton, editButton)
+    uList.append(newItem)
+    console.log(deleteButton)
+    
+    deleteButton.addEventListener("click", function(e){
+            e.target.parentNode.remove()
+    })
+})
