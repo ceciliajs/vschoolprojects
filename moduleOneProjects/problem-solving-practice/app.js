0ddc2a7c5b97d596ab1445fc6200107734041001
Cecilia <cecilia.stark@ibm.com>
2021-05-17 00:51:44 -0500

    homework update

---
+++ moduleOneProjects/problem-solving-practice/app.js
@@ -0,0 +1,39 @@
+// //Write a function that takes an array of numbers and returns the largest (without using Math.max())
+
+// var largestNum = 0;
+
+// function largest(arr){
+//     for (let index = 0;index <= arr.length - 1;index ++){
+//        // console.log(largestNum)
+//         //console.log(arr[index])
+//         if ( arr[index] > largestNum ){
+//             largestNum = arr[index]
+//         }
+        
+//     }
+//     console.log(largestNum)
+//     ////return i
+// };
+
+// // largest([1,	56,	67,	34,	79,	34,	78,	67,	45,	49]);
+// largest([3, 5, 2, 8, 1]) // => 8
+
+
+//Write a function that takes an array of words and a character and returns each word that has that character present.
+
+let emptyArr = []
+
+function lettersWithStrings(arr){
+    for (let index = 0; index <= arr.length; i ++){
+        if()
+    }
+}
+
+lettersWithStrings(["#3", "$$$", "C%4!", "Hey!"], "!") // => ["C%4!", "Hey!"]
+
+
+//Write a function that takes a num1 and num2 and returns whether num1 is divisible by num2.
+
+// isDivisible(4, 2) // => true
+// isDivisible(9, 3) // => true
+// isDivisible(15, 4) // => false
