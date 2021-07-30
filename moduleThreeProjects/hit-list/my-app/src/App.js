5eb42614f5cad5618dff978231b4bc35e981e2ed
Cecilia <cecilia.stark@ibm.com>
2021-07-30 09:49:06 -0500

    homework update

---
+++ moduleThreeProjects/hit-list/my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+//import "./style.css"
+
+import Hitlist from "./components/Hitlist.js"
+
+function App(){
+    return(
+        <div>
+            <Hitlist />
+        </div>
+    )
+
+}
+
+export default App
---
+++ moduleThreeProjects/hit-list/my-app/src/components/Hitlist.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react"
+import { render } from "react-dom"
+
+class Hitlist extends Component {
+    constructor() {
+        super()
+        this.state = {
+            hitListCards: []
+
+        }
+    }
+
+
+    componentDidMount() {
+        fetch(`https://raw.githubusercontent.com/VSchool/vschool-api/master/static/hitlist.json`)
+            .then(response => response.json())
+            .then(response => {
+                const { targets } = response.data
+                this.setState({ hitListCards: targets })
+            })
+    }
+
+
+    render() {
+        const targets = this.state.hitListCards.map((item,index)=>
+                <div>
+                    {item.img}
+                    {item.name}
+                </div>
+        )
+
+        return (
+
+
+            <div>
+                {targets}
+            </div>
+
+    )
+    }
+
+}
+export default Hitlist
